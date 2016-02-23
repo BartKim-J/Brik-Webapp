564214fea72eb4716e3d58829f1ab40f82809ab0
Nicklas <nil.nicklas@gmail.com>
2016-02-23 14:45:57 +0900

    Upadte typos

--- messages/data/jobOpenings.js
+++ messages/data/jobOpenings.js
@@ -4,14 +4,14 @@ let Immutable = require('seamless-immutable');
 
 module.exports = Immutable([{
   category: `Hardware`,
-  desc: `*Firmware Developer(1 person) - looking for an engineer who can program a core chip for Spendwallet. Experience in RF would be welcomed.`
+  desc: `*Firmware Developer(1 person)* - looking for an engineer who can program a core chip for Spendwallet. Experience in RF would be welcomed.`
 }, {
   category: `Software`,
-  desc: `*iOS Developer(1 person) - looking for an engineer who can develop iOS mobile application. Experience in Bluetooth and SWIFT would be welcomed.`
+  desc: `*iOS Developer(1 person)* - looking for an engineer who can develop iOS mobile application. Experience in Bluetooth and SWIFT would be welcomed.`
 }, {
   category: `Marketing`,
-  desc: `*Marketer(1 person) - looking for a marketer who can take charge of product branding and marketing strategy.`
+  desc: `*Marketer(1 person)* - looking for a marketer who can take charge of product branding and marketing strategy.`
 }, {
   category: `Operation`,
-  desc: `*Operator(1 person) - looking for an operator who can take charge of tax accounting work and team management.`
+  desc: `*Operator(1 person)* - looking for an operator who can take charge of tax accounting work and team management.`
 }]);
