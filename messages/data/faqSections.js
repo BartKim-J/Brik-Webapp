3252e83cfba4070d738a961f3a700300b473c6af
Nicklas Nilsson <nicklas@xengineering.co>
2016-03-21 11:27:49 +0900

    Updated FAQ section

--- messages/data/faqSections.js
+++ messages/data/faqSections.js
@@ -4,62 +4,83 @@ let Immutable = require('seamless-immutable');
 
 module.exports = Immutable({
   col1: [{
-    title: `General`,
+    title: `About Spendwallet`,
     entries: [{
       question: `What is Spendwallet?`,
-      answer: `Spendwallet is the world's first physical electronic wallet that pays with just a tap. With its magnetic flux emulation technology, Spendwallet can be used anywhere that accepts magnetic payment cards.`
+      answer: `Spendwallet is a cutting-edge smart wallet that digitally consolidates credit, debit and gift cards securely into one device.`
+    }, {
+      question: `Why do I need Spendwallet?`,
+      answer: `Too many cards. Too much hassle. If the reason for your thick wallet was a large number of cards with which you would like to have the utmost benefit, neither your smart spending nor your slim pocket should be given up. It is time for your heavy, thick traditional wallet to evolve into smart wallet that converts all those cards into digital information.`
     }, {
       question: `What types of cards can I store on Spendwallet?`,
-      answer: `As long as the backside of the cards has a magnetic stripe, you can store credit, debit, gift and all other types of cards into one device. The point cards with barcode can be saved on Spendwallet mobile application and easily used.`
+      answer: `As long as the backside of the cards has a magnetic stripe, you can store credit, debit, gift and all other types of cards into one device. The point cards with barcode can be saved on Spendwallet mobile application.`
     }, {
       question: `How many cards can be stored on Spendwallet?`,
       answer: `Up to 20 cards can be stored on Spendwallet.`
     }, {
+      question: `How long does the battery last?`,
+      answer: `When Spendwallet is fully charged, the battery lasts up to 30 days.`
+    }, {
+      question: `What mobile devices does Spendwallet support?`,
+      answer: `Spendwallet supports Android as well as Iphone.`
+    }]
+  },
+  {
+    title: `How to Use`,
+    entries: [{
       question: `How do I store my cards on Spendwallet?`,
-      answer: `First install Spendwallet mobile application and connect your Spendwallet reader to headphone jack. After you connect mobile phone and device via Bluetooth, swipe the payment cards smoothly through the reader to store your cards on Spendwallet.`
+      answer: `Install Spendwallet mobile application and connect your Spendwallet reader to headphone jack. Then, swipe the payment cards smoothly through the reader and sync with Spendwallet via Bluetooth to transfer card data to Spendwallet.`
     }, {
       question: `How can I select which card to use on Spendwallet?`,
-      answer: `With the help of the Spendwallet's left and right button, select which card to use and make sure whether your card's nickname is correctly appeared on LED display.`
+      answer: `Select which card to use using left and right button, and make sure whether your card’s nickname is correctly appeared on LED display.`
     }, {
       question: `How can I make a payment with Spendwallet?`,
-      answer: `After making sure whether the right card was selected on LED display, bring your Spendwallet near to the card reader and press “SPEND” button on Spendwallet to make a payment.`
+      answer: `Tap 'SPEND' button on Spendwallet and bring it close to the card reader.`
     }, {
       question: `How can I store my identification card on Spendwallet?`,
       answer: `You can store your identification card on backside slot of Spendwallet.`
     }, {
-      question: `What mobile devices does Spendwallet support?`,
-      answer: `Spendwallet suppots Android as well as Iphone.`
-    }, {
-      question: `How long does the battery last?`,
-      answer: `When Spendwallet is fully charged, the battery lasts up to 30 days.`
-    }, {
-      question: `Where can I purchase Spendwallet?`,
-      answer: `You can purchase Spendwallet at either our website(spendwallet.com) or Kickstarter.`
+      question: `Should I turn on Bluetooth while using Spendwallet?`,
+      answer: `Even when you do not turn on Bluetooth on your phone, you can still use your Spendwallet.`
     }]
   }],
   col2: [{
+    title: `Technology`,
+    entries: [{
+      question: `How is it possible to make a payment everywhere?`,
+      answer: `When you bring Spendwallet near to the card reader, our self-developed Magnetic Flux Emulation (MFE) technology generates magnetic field, which makes the card reader to respond as if a card were swiped. Hence, Spendwallet can be used anywhere that accepts magnetic payment cards.`
+    }, {
+      question: `How is Spendwallet different from all those all-in-one smart cards?`,
+      answer: `SpendWallet works better. We’ve tested all available smart cards in the market and concluded that the method of swiping cannot guarantee 100% reliability due to technological deficiency. Instead of swiping, SpendWallet adopts a tap-to-pay method, providing the highest coverage.`
+    }]
+  },
+  {
     title: `Security`,
     entries: [{
-      question: `How do I transfer my card information onto Spendwallet? Any security problems?`,
-      answer: `The information stored on Spendwallet mobile application will be safely transferred to Spendwallet device through Bluetooth connection. As the communication is completed according to Bluetooth standard security standard, there is no such problem with security.`
+      question: `Should I worry about security problem when transferring my card information onto Spendwallet?`,
+      answer: `No, the information stored on Spendwallet mobile application will be safely transferred to Spendwallet device according to Bluetooth standard security. Also, Spendwallet provides bank level (256 bit) encryption for all personal data.`
     }, {
       question: `What happens when I lose Spendwallet?`,
-      answer: `When your smartphone and Spendwallet is connected via Bluetooth, you don't need to worry about losing your wallet. When the Spendwallet is 20m away from your smarthpone, you will be kindly notified that you left your Spendwallet behind. Even if you did not have a Bluetooth connection and lost your Spendwallet, the passcode you first entered will safely protect Spendwallet from other's hands. In addition, when Spendwallet remains as lost mode for a certain period of time, all the information stored on Spendwallet will be completely deleted. The Spendwallet is safer than any other wallet, credit card, and other payment service, and thus safely protect your card information.`
+      answer: `When your smartphone and Spendwallet is connected via Bluetooth, and the Spendwallet is 20m away from your smarthpone, you will be kindly notified that you left your Spendwallet behind. In addition, Spendwallet is automatically locked and included data will be self-destructed when lost.`
     }, {
-      question: `How can I maintain SPEND's security without turning on Bluetooth on my phone?`,
-      answer: `Even when you did not turn on Bluetooth on your phone, you can still use your Spendwallet. If you prefer to turn off Bluetooth, you can either enter passcode for each transaction for security or simply turn off the security function.`
+      question: `How can I maintain Spendwallet's security without turning on Bluetooth on my phone?`,
+      answer: `If you prefer to turn off Bluetooth, you can either enter passcode for each transaction for security or simply turn off the security function.`
     }, {
       question: `What is passcode and how do I set it up?`,
-      answer: `passcode is the security equipment for you to safely protect your card information. You can set your passcode when you first open the mobile application. Since the passcode is set with the left and right button as well as SPEND button, the passcode can be identically used on Spendwallet.`
+      answer: `Passcode is the security equipment for you to safely protect your card information. As the passcode is set with the left and right button as well as SPEND button on mobile application, the passcode can be identically used on Spendwallet.`
     }, {
       question: `What if I forget the passcode?`,
       answer: `When you forget the passcode, you can reset your passcode on your mobile application after verifying yourself.`
-    }]
+    }
+  ]
   }, {
-    title: `Shipment`,
+    title: `Order & Shipping`,
     entries: [{
+      question: `Where can I purchase Spendwallet?`,
+      answer: `You can currently buy Spendwallet at our Indiegogo page only.`
+    }, {
       question: `When will I get my Spendwallet?`,
-      answer: `You will receive Spendwallet within 3 months. Though, we will try our best to ship the product to you as soon as possible. -> We estimate shipping no later than by April 2016.`
+      answer: `We estimate shipping no later than by August 2016.`
     }, {
       question: `Do you ship internationally?`,
       answer: `We do ship internationally, but shipping cost will vary depending on countries.`
